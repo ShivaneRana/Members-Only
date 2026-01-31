c37fb4dff6ebdf2858f5d380b1409a2ce5978bca
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-01-31 21:39:24 +0530

    feat: added functionality to render posts for db

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,3 +1,6 @@
-exports.renderPosts = (req, res) => {
-    res.status(200).render('posts');
+const db = require('../db/queries.js');
+
+exports.renderPosts = async (req, res) => {
+    const posts = await db.fetchPosts();
+    res.status(200).render('posts',{posts});
 };
