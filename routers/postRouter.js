c18ba18cf8090a0c590132cb5c5c031aea7cb925
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-01-31 19:51:45 +0530

    chore: router created for post

---
+++ routers/postRouter.js
@@ -0,0 +1,8 @@
+const { Router } = require('express');
+const postController = require("../controllers/postController.js");
+
+const postRouter = Router();
+
+postRouter.get('/',postController.renderPosts);
+
+module.exports = postRouter;
