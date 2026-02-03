cf741b57d36141d2e19715cff21ca0b2552d9f28
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-02-03 22:51:28 +0530

    feat: added post method route for /admin

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -8,13 +8,13 @@ const dotenv = require('dotenv').config({
 
 const { body, validationResult } = require('express-validator');
 
-const validationObject = [
+const validationObject_member = [
     body('membership_code')
         .trim()
         .notEmpty()
         .withMessage('Cannot be empty')
         .custom((value) => {
-            if (value === process.env.SECRET_CODE) {
+            if (value === process.env.SECRET_CODE_MEMBERSHIP) {
                 return true;
             } else {
                 throw new Error('Wrong code');
@@ -22,8 +22,22 @@ const validationObject = [
         }),
 ];
 
+const validationObject_admin = [
+    body('admin_code')
+        .trim()
+        .notEmpty()
+        .withMessage('Cannot be empty')
+        .custom((value) => {
+            if (value === process.env.SECRET_CODE_ADMIN) {
+                return true;
+            } else {
+                throw new Error('Wrong code');
+            }
+        }),
+]
+
 exports.makeMember = [
-    validationObject,
+    validationObject_member,
     (req, res) => {
         const errors = validationResult(req);
 
@@ -33,5 +47,19 @@ exports.makeMember = [
 
         db.enableMembership(req.user.mid);
         return res.status(200).redirect('/member');
+    },
+];
+
+exports.makeAdmin = [
+    validationObject_admin,
+    (req, res) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(200).render('admin', { errors: errors.array() });
+        }
+
+        db.enableAdmin(req.user.mid);
+        return res.status(200).redirect('/admin');
     },
 ];
