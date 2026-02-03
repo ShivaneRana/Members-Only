5e53f08de3d1a410c6bd26688432e97113c2c8ec
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-02-03 15:52:47 +0530

    fix(db): fixer typo in table name

--- db/populate.js
+++ db/populate.js
@@ -28,7 +28,7 @@ let createTable = `
         mid INT REFERENCES members(mid)
     );
 
-    CREATE TABLE IF NOT EXISTS member_sessions (
+    CREATE TABLE IF NOT EXISTS sessions (
     sid TEXT PRIMARY KEY,              
     sess JSONB NOT NULL,               
     expire TIMESTAMPTZ NOT NULL         
