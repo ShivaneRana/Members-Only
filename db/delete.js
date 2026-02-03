99b7e5719b20d41865f42db87c98974d8d360ece
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-02-03 15:52:18 +0530

    feat(database): added script for delete all tables

---
+++ db/delete.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+const pool = require('./pool.js');
+
+const deleteData = `
+    DELETE FROM members;
+    DELETE FROM global_chat;
+    DELETE FROM sessions;
+`;
+
+const deleteTable = `
+
+    DROP TABLE global_chat;
+    DROP TABLE members;
+    DROP TABLE member_sessions;
+    DROP TABLE sessions;
+
+`
+
+const deleteInfo = async () => {
+    console.log("Deleting tables...");
+    try{
+        await pool.query(deleteData);
+        await pool.query(deleteTable);
+        console.log("Data deleted.");
+    }catch(err){
+        console.log(1111)
+        console.error(err);
+    }
+}
+
+deleteInfo();
