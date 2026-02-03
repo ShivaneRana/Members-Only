143b28e9956c1bc1f55e9391b1bfeb19baf2b6be
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-02-03 11:12:10 +0530

    feat: 1. setup session store, session. 2. added route to logout 3. added route for user auththentication. 4. added middleware to attach req.user to res.locals.currentUser

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const path = require('node:path');
-const bycrypt = require('bcryptjs');
-const dotenv = require('dotenv').config({ quiet: true, debug: false });
+const pool = require('./db/pool.js');
+const passport = require('passport');
+const session = require('express-session');
+const psqlStore = require('connect-pg-simple')(session);
+const dotenv = require('dotenv').config({quiet: true,debug: false});
 
 // Routers
 const indexRouter = require('./routers/indexRouter.js');
 const signupRouter = require('./routers/signupRouter.js');
 const loginRouter = require('./routers/loginRouter.js');
 const postRouter = require('./routers/postRouter.js');
+const {configurePassport} = require('./middlewares/passport.js');
+const isAuth = require('./middlewares/isAuth.js')
+
+const SESSION_STORE = process.env.SESSION_STORE;
+const sessionStore = new psqlStore({pool : pool,tableName : 'sessions' ,pruneSessionInterval: 23 * 60 * 60});
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -15,10 +23,48 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  store: sessionStore,
+  secret: SESSION_STORE,
+  resave: false,
+  saveUninitialized:false,
+  cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }
+}));
+
+configurePassport(passport,pool);
+
+app.use(passport.initialize());
+app.use(passport.session())
+
+app.use((req,res,next) => {
+    res.locals.currentUser = req.user;
+    next();
+}) 
+
 app.use('/', indexRouter);
+
 app.use('/sign-up', signupRouter);
+
+app.post('/log-in',passport.authenticate("local", {
+    successRedirect: "/posts",
+    failureRedirect: "/"
+}));
+
 app.use('/log-in', loginRouter);
 app.use('/post{s}', postRouter);
+
+app.get("/log-out", (req, res, next) => {
+  req.logout((err) => {
+    if (err) return next(err);
+
+    req.session.destroy(err => {
+        if(err) return next(err);
+
+    res.clearCookie('connect.sid');
+    res.redirect("/");
+    })
+  });
+});
 
 app.use((req, res) => {
     res.status(404).render('404');
@@ -28,6 +74,7 @@ app.use((err, req, res, next) => {
     console.error(err);
     res.status(500).render('error');
 });
+
 
 app.listen(3000, (err) => {
     if (err) {
