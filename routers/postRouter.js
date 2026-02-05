cce23ebe1c8df23f9b068a7fab2e6d4bb9d59aaf
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-02-05 23:24:58 +0530

    feat: added route for post

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -4,5 +4,6 @@ const postController = require('../controllers/postController.js');
 const postRouter = Router();
 
 postRouter.get('/', postController.renderPosts);
+postRouter.post('/',postController.createPosts);
 
 module.exports = postRouter;
