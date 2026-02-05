12fdb0662e33356e069ea76de74029ec045b157b
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-02-05 23:24:22 +0530

    feat: created function to add post

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,5 +2,13 @@ const db = require('../db/queries.js');
 
 exports.renderPosts = async (req, res) => {
     const posts = await db.fetchPosts();
-    res.status(200).render('posts', { posts });
+    return res.status(200).render('posts', { posts });
 };
+
+exports.createPosts = async(req,res) => {
+    const post = req.body.post_text;
+    const {mid} = req.user;
+    await db.addPosts(mid,post);
+    return res.status(200).redirect("/posts");
+    // db.addPosts()
+}
