fa26e79597dafe312e5db64d1203ba5981c3b62a
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-02-09 22:12:55 +0530

    feat: added input validation

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,16 +1,34 @@
 const db = require('../db/queries.js');
+const {body,validationResult} = require('express-validator');
+
+const validationObject = [
+    body('post_text')
+        .trim()
+        .notEmpty()
+        .withMessage('Post cannot be empty')
+        .isLength({ max: 280 })
+        .withMessage('Character limit of 280 exceeded')
+]
 
 exports.renderPosts = async (req, res) => {
     const posts = await db.fetchPosts();
     return res.status(200).render('posts', { posts });
 };
 
-exports.createPosts = async (req, res) => {
-    const post = req.body.post_text;
-    const { mid } = req.user;
-    await db.addPosts(mid, post);
-    return res.status(200).redirect('/posts');
-};
+exports.createPosts = [
+    validationObject,
+    async (req,res) => {
+        const posts = await db.fetchPosts();
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(200).render('posts',{posts,errors: errors.array()})
+        }
+        const post = req.body.post_text;
+        const { mid } = req.user;
+        await db.addPosts(mid, post);
+        return res.status(200).redirect('/posts');
+    }
+]
 
 exports.deletePosts = async (req, res) => {
     if (!req.user) {
