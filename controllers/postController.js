d19f5c17a0c5577efb479675675fddcbc9899ccc
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-02-09 22:29:05 +0530

    chore: code formatted via prettier

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,5 +1,5 @@
 const db = require('../db/queries.js');
-const {body,validationResult} = require('express-validator');
+const { body, validationResult } = require('express-validator');
 
 const validationObject = [
     body('post_text')
@@ -7,8 +7,8 @@ const validationObject = [
         .notEmpty()
         .withMessage('Post cannot be empty')
         .isLength({ max: 280 })
-        .withMessage('Character limit of 280 exceeded')
-]
+        .withMessage('Character limit of 280 exceeded'),
+];
 
 exports.renderPosts = async (req, res) => {
     const posts = await db.fetchPosts();
@@ -17,18 +17,18 @@ exports.renderPosts = async (req, res) => {
 
 exports.createPosts = [
     validationObject,
-    async (req,res) => {
+    async (req, res) => {
         const posts = await db.fetchPosts();
         const errors = validationResult(req);
-        if(!errors.isEmpty()){
-            return res.status(200).render('posts',{posts,errors: errors.array()})
+        if (!errors.isEmpty()) {
+            return res.status(200).render('posts', { posts, errors: errors.array() });
         }
         const post = req.body.post_text;
         const { mid } = req.user;
         await db.addPosts(mid, post);
         return res.status(200).redirect('/posts');
-    }
-]
+    },
+];
 
 exports.deletePosts = async (req, res) => {
     if (!req.user) {
