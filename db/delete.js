9aea2c6f0182b301e80ade32790e09f22985ac9c
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-02-09 20:28:30 +0530

    fix: 1.removed entry from deleteTable string and removed useless log statement.

--- db/delete.js
+++ db/delete.js
@@ -2,16 +2,14 @@
 const pool = require('./pool.js');
 
 const deleteData = `
-    DELETE FROM members;
-    DELETE FROM global_chat;
     DELETE FROM sessions;
+    DELETE FROM global_chat;
+    DELETE FROM members;
 `;
 
 const deleteTable = `
-
     DROP TABLE global_chat;
     DROP TABLE members;
-    DROP TABLE member_sessions;
     DROP TABLE sessions;
 
 `;
@@ -23,7 +21,6 @@ const deleteInfo = async () => {
         await pool.query(deleteTable);
         console.log('Data deleted.');
     } catch (err) {
-        console.log(1111);
         console.error(err);
     }
 };
